dd9b41b31bd63ea689318f0f44285a07e7c5fbcd
DikshantJajedi <50725236+DikshantJajedi@users.noreply.github.com>
2020-11-11 16:12:10 +0530

    Update install_criu.sh

--- hack/install/install_criu.sh
+++ hack/install/install_criu.sh
@@ -16,7 +16,8 @@ criu::ubuntu::install_dependencies() {
     python-protobuf \
     libnl-3-dev \
     libcap-dev \
-    asciidoc
+    asciidoc \ 
+    xmlto
 }
 
 # criu::ubuntu::install will install criu from source.
