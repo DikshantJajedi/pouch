67b9d3efd91eae3cd59ee2131e6e08dd3fd21b01
DikshantJajedi <50725236+DikshantJajedi@users.noreply.github.com>
2020-11-12 17:07:07 +0530

    Update install_critest.sh

--- hack/install/install_critest.sh
+++ hack/install/install_critest.sh
@@ -48,15 +48,15 @@ critest::install() {
   echo ">>>> checkout <<<<"
   git checkout "${CRITEST_BRANCH_DEFAULT}"
   echo ">>>> make running <<<<"
-  #if [[ "${ARCH}" == "aarch64" ]]; then
-  #  wget -O critest https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.19.0/critest-v1.19.0-linux-arm64.tar.gz
-  #else
-  #  wget -O critest https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.19.0/critest-v1.19.0-linux-amd64.tar.gz
-  #fi
+  if [[ "${ARCH}" == "aarch64" ]]; then
+    wget -O critest https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.19.0/critest-v1.19.0-linux-arm64.tar.gz
+  else
+    wget -O critest https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.19.0/critest-v1.19.0-linux-amd64.tar.gz
+  fi
   #wget -O critest https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.19.0/critest-v1.19.0-linux-$(arch1).tar.gz
-  #tar -xvf critest
-  #cp critest /usr/local/bin/critest
-  make
+  tar -xvf critest
+  cp critest /usr/local/bin/critest
+  #make
   echo ">>>> make completed <<<<"
   cd -
   echo ">>>> DONEEEEEEEEEE <<<<"
